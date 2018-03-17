261864c17978f45c0a3f8ebc0485a5d06c53c486
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-03-16 18:30:59 -0700

    small fixes

--- cmd/rego/main.go
+++ cmd/rego/main.go
@@ -47,8 +47,36 @@ func main() {
 		},
 	}
 
+	var docCmd = &cobra.Command{
+		Use:   "doc",
+		Short: "Generate a documentation for the given specification set",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return viper.BindPFlags(cmd.Flags())
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			s, err := spec.NewSpecificationSet(
+				viper.GetString("dir"),
+				nil,
+				nil,
+				viper.GetString("category"),
+			)
+			if err != nil {
+				return err
+			}
+
+			return writeDoc(s, viper.GetString("format"), viper.GetString("out"))
+		},
+	}
+
+	docCmd.Flags().StringP("dir", "d", "", "Directory containing the specification")
+	docCmd.Flags().StringP("out", "o", "", "Directory where to write the documentation")
+	docCmd.Flags().String("format", "markdown", "Format of the documentation")
+	docCmd.Flags().String("category", "", "Category of the type mapping to look for")
+
 	rootCmd.AddCommand(
 		formatCmd,
+		docCmd,
 	)
 
 	if err := rootCmd.Execute(); err != nil {
