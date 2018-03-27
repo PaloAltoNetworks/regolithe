f94420822d772524ff95b271a931f61ec36be7cb
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-03-26 17:33:12 -0700

    new: added unit tests

--- spec/helpers_test.go
+++ spec/helpers_test.go
@@ -1,10 +1,29 @@
 package spec
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/xeipuuv/gojsonschema"
 )
+
+type mockResultError struct{}
+
+func (r *mockResultError) Field() string                        { return "field" }
+func (r *mockResultError) SetType(string)                       {}
+func (r *mockResultError) Type() string                         { return "type" }
+func (r *mockResultError) SetContext(*gojsonschema.JsonContext) {}
+func (r *mockResultError) Context() *gojsonschema.JsonContext   { return nil }
+func (r *mockResultError) SetDescription(string)                {}
+func (r *mockResultError) Description() string                  { return "description" }
+func (r *mockResultError) SetDescriptionFormat(string)          {}
+func (r *mockResultError) DescriptionFormat() string            { return "format" }
+func (r *mockResultError) SetValue(interface{})                 {}
+func (r *mockResultError) Value() interface{}                   { return nil }
+func (r *mockResultError) SetDetails(gojsonschema.ErrorDetails) {}
+func (r *mockResultError) Details() gojsonschema.ErrorDetails   { return gojsonschema.ErrorDetails{} }
+func (r *mockResultError) String() string                       { return "woops" }
 
 func TestHelpers_Pluralize(t *testing.T) {
 
@@ -144,6 +163,47 @@ func TestHelpers_Pluralize(t *testing.T) {
 
 			Convey("Then the word should be pluralized correctly", func() {
 				So(p, ShouldEqual, "ys")
+			})
+		})
+	})
+}
+
+func TestHelpers_makeSchemaValidationErrors(t *testing.T) {
+
+	Convey("Given I have some validation error", t, func() {
+
+		errs := []gojsonschema.ResultError{
+			&mockResultError{},
+		}
+
+		Convey("When I call makeSchemaValidationErrors", func() {
+
+			errs := makeSchemaValidationError("hello.spec", errs)
+
+			Convey("Then err should be correct", func() {
+				So(len(errs), ShouldEqual, 1)
+				So(errs[0].Error(), ShouldEqual, "hello.spec: schema error: woops")
+			})
+		})
+	})
+}
+
+func TestHelpers_formatValidationErrors(t *testing.T) {
+
+	Convey("Given I have some errors", t, func() {
+
+		errs := []error{
+			fmt.Errorf("1-err1"),
+			fmt.Errorf("0-err2"),
+			fmt.Errorf("2-err2"),
+		}
+
+		Convey("When I call formatValidationErrors", func() {
+
+			err := formatValidationErrors(errs)
+
+			Convey("Then err should be correct", func() {
+				So(err.Error(), ShouldEqual, "0-err2\n1-err1\n2-err2")
 			})
 		})
 	})
--- spec/relation_test.go
+++ spec/relation_test.go
@@ -27,3 +27,106 @@ func TestRelation_Getters(t *testing.T) {
 		})
 	})
 }
+
+func TestRelation_Validate(t *testing.T) {
+
+	Convey("Given I have a relation with no validation error", t, func() {
+
+		r := &Relation{
+			AllowsGet:    true,
+			AllowsCreate: true,
+			AllowsUpdate: true,
+			AllowsDelete: true,
+			RestName:     "remote",
+			Descriptions: map[string]string{
+				"get":    "hello.",
+				"create": "hello.",
+				"update": "hello.",
+				"delete": "hello.",
+			},
+		}
+
+		Convey("When I call validate", func() {
+
+			errs := r.Validate()
+
+			Convey("Then there should be no validation error", func() {
+				So(len(errs), ShouldEqual, 0)
+			})
+		})
+	})
+
+	Convey("Given I have a relation with all missing description", t, func() {
+
+		r := &Relation{
+			AllowsGet:    true,
+			AllowsCreate: true,
+			AllowsUpdate: true,
+			AllowsDelete: true,
+			RestName:     "remote",
+			currentSpecification: &Specification{
+				Model: &Model{
+					RestName: "currentSpec",
+				},
+			},
+			remoteSpecification: &Specification{
+				Model: &Model{
+					RestName: "remoteSpec",
+				},
+			},
+		}
+
+		Convey("When I call validate", func() {
+
+			errs := r.Validate()
+
+			Convey("Then there should be some validation errors", func() {
+				So(len(errs), ShouldEqual, 4)
+				So(errs[0].Error(), ShouldEqual, "currentSpec.spec: relation 'get' to 'remote' must have a description")
+				So(errs[1].Error(), ShouldEqual, "currentSpec.spec: relation 'create' to 'remote' must have a description")
+				So(errs[2].Error(), ShouldEqual, "currentSpec.spec: relation 'update' to 'remote' must have a description")
+				So(errs[3].Error(), ShouldEqual, "currentSpec.spec: relation 'delete' to 'remote' must have a description")
+			})
+		})
+	})
+
+	Convey("Given I have a relation with all missing final period", t, func() {
+
+		r := &Relation{
+			AllowsGet:    true,
+			AllowsCreate: true,
+			AllowsUpdate: true,
+			AllowsDelete: true,
+			RestName:     "remote",
+			Descriptions: map[string]string{
+				"get":    "hello",
+				"create": "hello",
+				"update": "hello",
+				"delete": "hello",
+			},
+			currentSpecification: &Specification{
+				Model: &Model{
+					RestName: "currentSpec",
+				},
+			},
+			remoteSpecification: &Specification{
+				Model: &Model{
+					RestName: "remoteSpec",
+				},
+			},
+		}
+
+		Convey("When I call validate", func() {
+
+			errs := r.Validate()
+
+			Convey("Then there should be some validation errors", func() {
+				So(len(errs), ShouldEqual, 4)
+				So(errs[0].Error(), ShouldEqual, "currentSpec.spec: relation 'get' to 'remote' description must end with a period")
+				So(errs[1].Error(), ShouldEqual, "currentSpec.spec: relation 'create' to 'remote' description must end with a period")
+				So(errs[2].Error(), ShouldEqual, "currentSpec.spec: relation 'update' to 'remote' description must end with a period")
+				So(errs[3].Error(), ShouldEqual, "currentSpec.spec: relation 'delete' to 'remote' description must end with a period")
+			})
+		})
+	})
+}
