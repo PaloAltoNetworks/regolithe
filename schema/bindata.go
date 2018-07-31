09a5b4b802ac16e304ad126e63804e9b297554d6
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-07-31 12:46:40 -0700

    update bindata

--- schema/bindata.go
+++ schema/bindata.go
@@ -130,7 +130,7 @@ func regoAbstractJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "rego-abstract.json", size: 9559, mode: os.FileMode(420), modTime: time.Unix(1533065407, 0)}
+	info := bindataFileInfo{name: "rego-abstract.json", size: 9559, mode: os.FileMode(420), modTime: time.Unix(1533066350, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -210,7 +210,7 @@ func regoSpecJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "rego-spec.json", size: 17282, mode: os.FileMode(420), modTime: time.Unix(1533065407, 0)}
+	info := bindataFileInfo{name: "rego-spec.json", size: 17282, mode: os.FileMode(420), modTime: time.Unix(1533066350, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
