c32d4ba7950f1369115ea1a6ad235c3bb1575e17
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-08-01 13:58:48 -0700

    fixed: full support for default values

--- spec/parameter.go
+++ spec/parameter.go
@@ -3,7 +3,9 @@ package spec
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/araddon/dateparse"
 	"gopkg.in/yaml.v2"
 )
 
@@ -102,24 +104,70 @@ func (p *Parameter) Validate(relatedReSTName string) []error {
 
 	if p.Type == "" {
 		errs = append(errs, fmt.Errorf("%s.spec: type of parameter '%s' must be set", relatedReSTName, p.Name))
-	}
+	} else {
 
-	if p.Type != ParameterTypeString &&
-		p.Type != ParameterTypeInt &&
-		p.Type != ParameterTypeFloat &&
-		p.Type != ParameterTypeBool &&
-		p.Type != ParameterTypeTime &&
-		p.Type != ParameterTypeDuration &&
-		p.Type != ParameterTypeEnum {
-		errs = append(errs, fmt.Errorf("%s.spec: type of parameter '%s' must be 'string', 'integer', 'float', 'boolean', 'enum', 'time' or 'duration'", relatedReSTName, p.Name))
+		if p.Type != ParameterTypeString &&
+			p.Type != ParameterTypeInt &&
+			p.Type != ParameterTypeFloat &&
+			p.Type != ParameterTypeBool &&
+			p.Type != ParameterTypeTime &&
+			p.Type != ParameterTypeDuration &&
+			p.Type != ParameterTypeEnum {
+			errs = append(errs, fmt.Errorf("%s.spec: type of parameter '%s' must be 'string', 'integer', 'float', 'boolean', 'enum', 'time' or 'duration'", relatedReSTName, p.Name))
+		}
 	}
 
 	if p.Type == ParameterTypeEnum && len(p.AllowedChoices) == 0 {
 		errs = append(errs, fmt.Errorf("%s.spec: enum parameter '%s' must define allowed_choices", relatedReSTName, p.Name))
 	}
 
+	if p.Type != ParameterTypeEnum && len(p.AllowedChoices) > 0 {
+		errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is not an enum but defines allowed_choices", relatedReSTName, p.Name))
+	}
+
 	if p.DefaultValue == nil && p.ExampleValue == nil && p.Type == ParameterTypeString {
 		errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' must provide an example value as it doesn't have a default", relatedReSTName, p.Name))
+	}
+
+	if p.DefaultValue != nil {
+		switch p.Type {
+		case ParameterTypeString:
+			if _, ok := p.DefaultValue.(string); !ok {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an string, but the default value is not", relatedReSTName, p.Name))
+			}
+		case ParameterTypeEnum:
+			if _, ok := p.DefaultValue.(string); !ok {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an enum, but the default value is not", relatedReSTName, p.Name))
+			}
+		case ParameterTypeInt:
+			if _, ok := p.DefaultValue.(int); !ok {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an integer, but the default value is not", relatedReSTName, p.Name))
+			}
+		case ParameterTypeFloat:
+			if _, ok := p.DefaultValue.(float64); !ok {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an float, but the default value is not", relatedReSTName, p.Name))
+			}
+		case ParameterTypeBool:
+			if _, ok := p.DefaultValue.(bool); !ok {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an boolean, but the default value is not", relatedReSTName, p.Name))
+			}
+		case ParameterTypeDuration:
+			if _, ok := p.DefaultValue.(string); !ok {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an duration, but the default value is not", relatedReSTName, p.Name))
+				break
+			}
+			if _, err := time.ParseDuration(p.DefaultValue.(string)); err != nil {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an duration, but the default value is not", relatedReSTName, p.Name))
+			}
+		case ParameterTypeTime:
+			if _, ok := p.DefaultValue.(string); !ok {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an time, but the default value is not", relatedReSTName, p.Name))
+				break
+			}
+			if _, err := dateparse.ParseAny(p.DefaultValue.(string)); err != nil {
+				errs = append(errs, fmt.Errorf("%s.spec: parameter '%s' is defined as an time, but the default value is not", relatedReSTName, p.Name))
+			}
+		}
 	}
 
 	return errs
