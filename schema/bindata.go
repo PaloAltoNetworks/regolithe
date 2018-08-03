c00fb017938e9e1d466819c90360a9e1844ab3a7
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-08-03 16:33:54 -0700

    pulish extension

--- schema/bindata.go
+++ schema/bindata.go
@@ -134,7 +134,7 @@ func regoAbstractJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "rego-abstract.json", size: 9570, mode: os.FileMode(420), modTime: time.Unix(1533336332, 0)}
+	info := bindataFileInfo{name: "rego-abstract.json", size: 9570, mode: os.FileMode(420), modTime: time.Unix(1533339211, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -234,7 +234,7 @@ func regoSharedParamsJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "rego-shared-params.json", size: 4433, mode: os.FileMode(420), modTime: time.Unix(1533336332, 0)}
+	info := bindataFileInfo{name: "rego-shared-params.json", size: 4433, mode: os.FileMode(420), modTime: time.Unix(1533339211, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -274,7 +274,7 @@ func regoSpecJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "rego-spec.json", size: 19574, mode: os.FileMode(420), modTime: time.Unix(1533336332, 0)}
+	info := bindataFileInfo{name: "rego-spec.json", size: 19574, mode: os.FileMode(420), modTime: time.Unix(1533339211, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
