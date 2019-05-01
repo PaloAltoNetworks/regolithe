f2a247bf4c1256c5a71961351267fea3029f9976
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-05-01 11:18:04 -0700

    new: licensing

--- doc.go
+++ doc.go
@@ -9,26 +9,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package jsonschema
-
-import (
-	"go.aporeto.io/regolithe/spec"
-)
-
-// Generate generates the json schema
-func Generate(set spec.SpecificationSet, outFolder string) error {
-
-	if err := writeModel(set, outFolder); err != nil {
-		return err
-	}
-
-	if err := writeGlobalResources(set, outFolder); err != nil {
-		return err
-	}
-
-	if err := writeGlobalResourceLists(set, outFolder); err != nil {
-		return err
-	}
-
-	return nil
-}
+// Package regolithe contains a base cobra.Command you can
+// use to write generator tools.
+package regolithe // import "go.aporeto.io/regolithe"
--- spec/doc.go
+++ spec/doc.go
@@ -9,26 +9,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package jsonschema
-
-import (
-	"go.aporeto.io/regolithe/spec"
-)
-
-// Generate generates the json schema
-func Generate(set spec.SpecificationSet, outFolder string) error {
-
-	if err := writeModel(set, outFolder); err != nil {
-		return err
-	}
-
-	if err := writeGlobalResources(set, outFolder); err != nil {
-		return err
-	}
-
-	if err := writeGlobalResourceLists(set, outFolder); err != nil {
-		return err
-	}
-
-	return nil
-}
+// Package spec contains the Go representation
+// of an Regolithe Specification Set as well as
+// various functions to handle reading and writing them.
+package spec // import "go.aporeto.io/regolithe/spec"
